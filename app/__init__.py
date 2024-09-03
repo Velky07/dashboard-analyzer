7aa6a7e5d3217bec54af86ebb45030d9be00f554
Jvctr <jvvilela_pe@hotmail.com>
2024-09-03 03:00:38 -0300

    Site em funcionamento - 0 responsivo

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
+import os
 
-app= Flask(__name__)
+app= Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'), static_folder=os.path.join(os.getcwd(), 'static'))
 
 from app import routes
